b7832a5b7e240c0f4de8f0b1eedb4f1abbe6ff8b
Wang Lin <wlin1@marvell.com>
2024-01-15 11:11:26 +0000

    Enable arm64/arm64 single platform dir
    Signed-off-by: Pavan Naregundi <pnaregundi@marvell.com>

--- platform/marvell/docker-syncd-mrvl.mk
+++ platform/marvell/docker-syncd-mrvl.mk
@@ -1,7 +1,7 @@
 # docker image for mrvl syncd
 
 DOCKER_SYNCD_PLATFORM_CODE = mrvl
-include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
+include $(PLATFORM_PATH)/../template/docker-syncd-bullseye.mk
 
 $(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD)
 
@@ -14,4 +14,3 @@ $(DOCKER_SYNCD_BASE)_VERSION = 1.0.0
 $(DOCKER_SYNCD_BASE)_PACKAGE_NAME = syncd
 
 $(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
-$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /var/run/docker-syncd:/var/run/sswsyncd
