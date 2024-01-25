8e85eb1fdc39cd63b8b2c3874d7aab7286d02e35
Pavan Naregundi <pnaregundi@marvell.com>
2024-01-25 11:01:38 +0000

    Add marvel-vs platform
    Signed-off-by: Pavan Naregundi <pnaregundi@marvell.com>

--- platform/marvell/rules.mk
+++ platform/marvell/rules.mk
@@ -3,6 +3,7 @@ include $(PLATFORM_PATH)/sai.mk
 include $(PLATFORM_PATH)/docker-syncd-mrvl.mk
 include $(PLATFORM_PATH)/docker-syncd-mrvl-rpc.mk
 include $(PLATFORM_PATH)/docker-saiserver-mrvl.mk
+include $(PLATFORM_PATH)/docker-syncd-mrvl-vs.mk
 include $(PLATFORM_PATH)/libsaithrift-dev.mk
 include $(PLATFORM_PATH)/one-image.mk
 ifeq ($(CONFIGURED_ARCH),arm64)
@@ -10,11 +11,14 @@ include $(PLATFORM_PATH)/mrvl-prestera.mk
 endif
 include $(PLATFORM_PATH)/platform-nokia.mk
 include $(PLATFORM_PATH)/platform-marvell.mk
+include $(PLATFORM_PATH)/onie.mk
+include $(PLATFORM_PATH)/kvm-image.mk
 
 SONIC_ALL += $(SONIC_ONE_IMAGE) \
              $(DOCKER_FPM) 	\
-             $(DOCKER_PTF_MRVL) \
-             $(DOCKER_SYNCD_MRVL_RPC)
+             $(DOCKER_SYNCD_MRVL_RPC) \
+             $(DOCKER_SYNCD_MRVL_VS) \
+	     $(SONIC_KVM_IMAGE)
 
 # Inject mrvl sai into syncd
 $(SYNCD)_DEPENDS += $(MRVL_SAI)
@@ -23,6 +27,3 @@ $(SYNCD)_UNINSTALLS += $(MRVL_SAI)
 ifeq ($(ENABLE_SYNCD_RPC),y)
 $(SYNCD)_DEPENDS += $(LIBSAITHRIFT_DEV)
 endif
-
-# Runtime dependency on mrvl sai is set only for syncd
-$(SYNCD)_RDEPENDS += $(MRVL_SAI)
--- platform/marvell/sai.mk
+++ platform/marvell/sai.mk
@@ -1,14 +1,21 @@
 # Marvell SAI
 
+MRVL_VSSAI_VERSION = 1.13.0-1
 ifeq ($(CONFIGURED_ARCH),arm64)
 MRVL_SAI_VERSION = 1.12.0-2
 else
 MRVL_SAI_VERSION = 1.13.0-1
 endif
 export $(MRVL_SAI_VERSION)
-export MRVL_SAI = mrvllibsai_$(MRVL_SAI_VERSION)_$(PLATFORM_ARCH).deb
+MRVL_SAI = mrvllibsai_$(MRVL_SAI_VERSION)_$(PLATFORM_ARCH).deb
+$(MRVL_SAI)_URL = "https://github.com/Marvell-switching/sonic-marvell-binaries/raw/master/$(CONFIGURED_ARCH)/sai-plugin/$(MRVL_SAI)"
 
-$(MRVL_SAI)_SRC_PATH = $(PLATFORM_PATH)/sai
 $(eval $(call add_conflict_package,$(MRVL_SAI),$(LIBSAIVS_DEV)))
+SONIC_ONLINE_DEBS += $(MRVL_SAI)
+$(MRVL_SAI)_SKIP_VERSION=y
 
-SONIC_MAKE_DEBS += $(MRVL_SAI)
+MRVL_VS_SAI = mrvlvslibsai_$(MRVL_VSSAI_VERSION)_$(PLATFORM_ARCH).deb
+$(MRVL_VS_SAI)_URL = "https://github.com/Marvell-switching/sonic-marvell-binaries/raw/master/native/sai-plugin/$(MRVL_VS_SAI)"
+
+SONIC_ONLINE_DEBS += $(MRVL_VS_SAI)
+$(MRVL_VS_SAI)_SKIP_VERSION=y
